d037f92e9235ef7366a2fdf17a068b9251f0fbd1
nikolay.mosyagin <nikolaymosyagin94@gmail.com>
2017-04-29 14:38:56 +0300

    Добавил неполный сценарий на добавление/удаление хранилища

---
+++ database/deletion_script_for_storage.sql
@@ -0,0 +1,11 @@
+drop sequence attribute_type_seq;
+drop sequence object_type_seq;
+drop sequence operation_seq;
+drop sequence object_seq;
+drop sequence attribute_seq;
+
+drop trigger attribute_type_trg;
+drop table attribute_type;
+
+drop trigger object_type_trg;
+drop table object_type;
